7fc521ac7e1f67b5ccfb755b4bc323485a9309a6
mykytaYaromenko <mykyta.yaromenko@onsiteai.onmicrosoft.com>
2025-04-09 18:58:52 +0200

    feat: pass pylint

--- backend/tools/mock_gpio.py
+++ backend/tools/mock_gpio.py
@@ -3,37 +3,16 @@ This file contains code sourced from the Mock.GPIO project:
 https://github.com/codenio/Mock.GPIO
 
 All credit for the original implementation goes to the original authors of that repository.
-This code is included here for reference, testing, or educational purposes and has not been written by me.
+This code is included here for reference, testing, or educational
+purposes and has not been written by me.
 
 License and ownership details remain with the original creators.
 """
 
-import time
-import logging
-import os
+from backend.tools.logger import Logger
 
-logger = logging.getLogger(__name__)
+logger = Logger.get_logger(__name__)
 
-log_level = os.getenv("LOG_LEVEL")
-
-if log_level is not None:
-    if log_level.lower() == "info":
-        logger.setLevel(logging.INFO)
-    if log_level.lower() == "debug":
-        logger.setLevel(logging.DEBUG)
-    if log_level.lower() == "warning":
-        logger.setLevel(logging.WARNING)
-    if log_level.lower() == "error":
-        logger.setLevel(logging.ERROR)
-    if log_level.lower() == "critical":
-        logger.setLevel(logging.CRITICAL)
-else:
-    logger.setLevel(logging.ERROR)
-
-stream_formatter = logging.Formatter("%(asctime)s:%(levelname)s: %(message)s")
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(stream_formatter)
-logger.addHandler(stream_handler)
 
 BCM = 11
 BOARD = 10
@@ -49,6 +28,7 @@ PUD_DOWN = 21
 PUD_OFF = 20
 PUD_UP = 22
 RISING = 31
+
 RPI_INFO = {
     "MANUFACTURER": "Sony",
     "P1_REVISION": 3,
@@ -63,20 +43,30 @@ SPI = 41
 UNKNOWN = -1
 VERSION = "0.7.0"
 
-_mode = 0
-
+_MODE = 0
 channel_config = {}
 
-# flags
-setModeDone = False
+# Flags
+SET_MODE_DONE = False
 
 
 class Channel:
+    """Represents a GPIO channel configuration."""
+
     def __init__(self, channel, direction, initial=0, pull_up_down=PUD_OFF):
-        self.channel = channel
+        self.channel_number = channel
         self.direction = direction
         self.initial = initial
         self.pull_up_down = pull_up_down
+
+    def __repr__(self):
+        return (
+            f"Channel(channel={self.channel_number}, direction={self.direction}, "
+            f"initial={self.initial}, pull_up_down={self.pull_up_down})"
+        )
+
+    def __str__(self):
+        return f"Channel {self.channel_number} configured as {self.direction}"
 
 
 # GPIO LIBRARY Functions
@@ -86,15 +76,14 @@ def setmode(mode):
     BOARD - Use Raspberry Pi board numbers
     BCM   - Use Broadcom GPIO 00..nn numbers
     """
-    # GPIO = GPIO()
+    global SET_MODE_DONE, _MODE
     if mode == BCM:
-        setModeDone = True
-        _mode = mode
-
+        SET_MODE_DONE = True
+        _MODE = mode
     elif mode == BOARD:
-        setModeDone = True
+        SET_MODE_DONE = True
     else:
-        setModeDone = False
+        SET_MODE_DONE = False
 
 
 def getmode():
@@ -102,78 +91,58 @@ def getmode():
     Get numbering mode used for channel numbers.
     Returns BOARD, BCM or None
     """
-    return _mode
+    return _MODE
 
 
 def setwarnings(flag):
     """
-    Enable or disable warning messages
+    Enable or disable warning messages.
     """
-    logger.info("Set warnings as {}".format(flag))
+    logger.info("Set warnings as %s", flag)
 
 
 def setup(channels, direction, initial=0, pull_up_down=PUD_OFF):
     """
-    Set up a GPIO channel or list of channels with a direction and (optional) pull up/down control
-    channels        - either board pin number or BCM number depending on which mode is set.
-    direction      - IN or OUT
-    [pull_up_down] - PUD_OFF (default), PUD_UP or PUD_DOWN
-    [initial]      - Initial value for an output channel
-
+    Set up a GPIO channel or list of channels with a direction and (optional) pull up/down control.
     """
-    global channel_config
-
-    if type(channels) is list or type(channels) is tuple:
+    if isinstance(channels, (list, tuple)):
         for channel in channels:
             logger.info(
-                "Setup channel : {} as {} with initial :{} and pull_up_down {}".format(
-                    channel, direction, initial, pull_up_down
-                )
+                "Setup channel: %s as %s with initial: %s and pull_up_down: %s",
+                channel,
+                direction,
+                initial,
+                pull_up_down,
             )
             channel_config[channel] = Channel(channel, direction, initial, pull_up_down)
     else:
         logger.info(
-            "Setup channel : {} as {} with initial :{} and pull_up_down {}".format(
-                channels, direction, initial, pull_up_down
-            )
+            "Setup channel: %s as %s with initial: %s and pull_up_down: %s",
+            channels,
+            direction,
+            initial,
+            pull_up_down,
         )
         channel_config[channels] = Channel(channels, direction, initial, pull_up_down)
 
 
-def output(channels, values):
+def output(channel: int, values: int):
     """
-    Output to a GPIO channel or list of channels
-    channels - either board pin number or BCM number depending on which mode is set.
+    Output to a GPIO channel
+
+    channel - either board pin or BCM number depending on which mode is set.
     values   - 0/1 or False/True or LOW/HIGH
 
     """
-    if type(channels) is list or type(channels) is tuple:
-        for channel in channels:
-            if type(values) is list or type(values) is tuple:
-                for value in values:
-                    logger.info(
-                        "Output channel : {} with value : {}".format(channel, value)
-                    )
-            else:
-                logger.info(
-                    "Output channel : {} with value : {}".format(channel, values)
-                )
-    else:
-        if type(values) is list or type(values) is tuple:
-            for value in values:
-                logger.info(
-                    "Output channel : {} with value : {}".format(channels, value)
-                )
-        else:
-            logger.info("Output channel : {} with value : {}".format(channels, values))
+    logger.info("Output channel : %s with value : %s", channel, values)
 
 
-def input(channel):
+def gpio_input(channel):
     """
     Input from a GPIO channel.  Returns HIGH=1=True or LOW=0=False
-    channel - either board pin number or BCM number depending on which mode is set.
+    channel - either board pin or BCM number depending on which mode is set.
     """
-    logger.info("Reading from channel {}".format(channel))
+    logger.info("Reading from channel %s", channel)
 
 
 def wait_for_edge(channel, edge, bouncetime, timeout):
@@ -185,142 +154,140 @@ def wait_for_edge(channel, edge, bouncetime, timeout):
     [timeout]    - timeout in ms
     """
     logger.info(
-        "Waiting for edge : {} on channel : {} with bounce time : {} and Timeout :{}".format(
-            edge, channel, bouncetime, timeout
-        )
+        "Waiting for edge : %s on channel : %s with bounce time : %s and Timeout :%s",
+        edge,
+        channel,
+        bouncetime,
+        timeout,
     )
 
 
 def add_event_detect(channel, edge, callback=None, bouncetime=None):
     """
     Enable edge detection events for a particular GPIO channel.
-    channel      - either board pin number or BCM number depending on which mode is set.
+    channel      - either board pin or BCM number depending on which mode is set.
     edge         - RISING, FALLING or BOTH
     [callback]   - A callback function for the event (optional)
     [bouncetime] - Switch bounce timeout in ms for callback
     """
     logger.info(
-        "Event detect added for edge : {} on channel : {} with bounce time : {} and callback {}".format(
-            edge, channel, bouncetime, callback
-        )
+        "Event detect added for edge : %s on channel : %s with bounce time : %s and callback %s",
+        edge,
+        channel,
+        bouncetime,
+        callback,
     )
 
 
 def event_detected(channel):
     """
-    Returns True if an edge has occurred on a given GPIO.  You need to enable edge detection using add_event_detect() first.
-    channel - either board pin number or BCM number depending on which mode is set.
+    Returns True if an edge has occurred on a given GPIO.
+    You need to enable edge detection using add_event_detect() first.
+    channel - either board pin or BCM number depending on which mode is set.
     """
-    logger.info("Waiting for even detection on channel :{}".format(channel))
+    logger.info("Waiting for even detection on channel :%s", channel)
 
 
 def add_event_callback(channel, callback):
     """
     Add a callback for an event already defined using add_event_detect()
-    channel      - either board pin number or BCM number depending on which mode is set.
+    channel      - either board pin or BCM number depending on which mode is set.
     callback     - a callback function
     """
-    logger.info("Event callback : {} added for channel : {}".format(callback, channel))
+    logger.info("Event callback : %s added for channel : %s", callback, channel)
 
 
 def remove_event_detect(channel):
     """
     Remove edge detection for a particular GPIO channel
-    channel - either board pin number or BCM number depending on which mode is set.
+
+    channel - either board pin or BCM number depending on which mode is set.
     """
-    logger.info("Event detect removed for channel : {}".format(channel))
+    logger.info("Event detect removed for channel : %s", channel)
 
 
 def gpio_function(channel):
     """
     Return the current GPIO function (IN, OUT, PWM, SERIAL, I2C, SPI)
-    channel - either board pin number or BCM number depending on which mode is set.
+    channel - either board pin or BCM number depending on which mode is set.
     """
     logger.info(
-        "GPIO function of channel : {} is {}".format(
-            channel, channel_config[channel].direction
-        )
+        "GPIO function of channel : %s is %s",
+        channel,
+        channel_config[channel].direction,
     )
 
 
 class PWM:
-    # initialise PWM channel
+    """Represents a PWM channel."""
+
     def __init__(self, channel, frequency):
         """
-        x.__init__(...) initializes x; see help(type(x)) for signature
+        Initialize PWM channel.
         """
         self.channel = channel
         self.frequency = frequency
         self.dutycycle = 0
-        global channel_config
-        channel_config[channel] = Channel(
-            channel,
-            PWM,
-        )
+        channel_config[channel] = Channel(channel, HARD_PWM)
         logger.info(
-            "Initialized PWM for channel : {} at frequency : {}".format(
-                channel, frequency
-            )
+            "Initialized PWM for channel: %s at frequency: %s", channel, frequency
         )
 
-    # where dc is the duty cycle (0.0 <= dc <= 100.0)
     def start(self, dutycycle):
         """
-        Start software PWM
-        dutycycle - the duty cycle (0.0 to 100.0)
+        Start software PWM.
         """
         self.dutycycle = dutycycle
         logger.info(
-            "Start pwm on channel : {} with duty cycle : {}".format(
-                self.channel, dutycycle
-            )
+            "Start PWM on channel: %s with duty cycle: %s", self.channel, dutycycle
         )
 
-    # where freq is the new frequency in Hz
-    def ChangeFrequency(self, frequency):
+    def change_frequency(self, frequency):
         """
-        Change the frequency
-        frequency - frequency in Hz (freq > 1.0)
+        Change the frequency.
         """
         logger.info(
-            "Freqency changed for channel : {} from : {} -> to : {}".format(
-                self.channel, self.frequency, frequency
-            )
+            "Frequency changed for channel: %s from: %s -> to: %s",
+            self.channel,
+            self.frequency,
+            frequency,
         )
         self.frequency = frequency
 
-    # where 0.0 <= dc <= 100.0
-    def ChangeDutyCycle(self, dutycycle):
+    def change_duty_cycle(self, dutycycle):
         """
-        Change the duty cycle
-        dutycycle - between 0.0 and 100.0
+        Change the duty cycle.
         """
         self.dutycycle = dutycycle
         logger.info(
-            "Dutycycle changed for channel : {} from : {} -> to : {}".format(
-                self.channel, self.dutycycle, dutycycle
-            )
+            "Duty cycle changed for channel: %s from: %s -> to: %s",
+            self.channel,
+            self.dutycycle,
+            dutycycle,
         )
 
-    # stop PWM generation
     def stop(self):
+        """
+        Stop PWM generation.
+        """
         logger.info(
-            "Stop PWM on channel : {} with duty cycle : {}".format(
-                self.channel, self.dutycycle
-            )
+            "Stop PWM on channel: %s with duty cycle: %s", self.channel, self.dutycycle
         )
 
 
 def cleanup(channels=None):
     """
-    Clean up by resetting all GPIO channels that have been used by this program to INPUT with no pullup/pulldown and no event detection
-    [channels] - individual channel or list/tuple of channels to clean up.  Default - clean every channel that has been used.
+    Clean up by resetting all GPIO channels that have been used by this program to
+    INPUT with no pullup/pulldown and no event detection
+
+    [channels] - individual channel or list/tuple of channels to clean up.
+    Default - clean every channel that has been used.
     """
     if channels is not None:
-        if type(channels) is list or type(channels) is tuple:
+        if isinstance(channels, (list, tuple)):
             for channel in channels:
-                logger.info("Cleaning up channel : {}".format(channel))
+                logger.info("Cleaning up channel : %s", channel)
         else:
-            logger.info("Cleaning up channel : {}".format(channels))
+            logger.info("Cleaning up channel : %s", channels)
     else:
         logger.info("Cleaning up all channels")
